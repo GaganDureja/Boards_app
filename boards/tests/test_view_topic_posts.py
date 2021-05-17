09f990189e536db34ba62a7c06cf0c66adb2efc7
GaganDureja <gagandureja675@gmail.com>
2021-05-17 22:14:45 +0530

    pagination seperated for include

--- boards/tests/test_view_topic_posts.py
+++ boards/tests/test_view_topic_posts.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from ..models import Board, Post, Topic
-from ..views import topic_posts
+from ..views import PostListView
 
 
 class TopicPostsTests(TestCase):
@@ -16,8 +16,8 @@ class TopicPostsTests(TestCase):
         self.response = self.client.get(url)
 
     def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)    
 
     def test_view_function(self):
         view = resolve('/boards/1/topics/1/')
-        self.assertEquals(view.func, topic_posts)
+        self.assertEquals(view.func.view_class, PostListView)
